d302237e7aafd0feef81443c22f5150cd833b3df
roboter5123 <pascal.roboter@hotmail.com>
2023-02-10 09:42:44 +0100

    implemented schwedule deletzion and showing on the dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import Login from "./loggedout/Login";
+import Register from "./loggedout/Register";
+import Dashboard from "./loggedin/Dashboard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" rel="stylesheet"/>
+            <Routes>
+                <Route path="/" element={<Dashboard rightButtonHandler={logout}/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
+            </Routes>
+        </div>
+    )
+}
+
+function logout() {
+
+    fetch("access-token", {
+        method: 'DELETE',
+        credentials: "include",
+    }).then((response) => {
+
+        if (response.ok) {
+
+            document.cookie = "login= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+            window.location.replace("/login")
+        }
+    })
+
+
 }
 
 export default App;
---
+++ src/loggedin/Dashboard.js
@@ -0,0 +1,51 @@
+import React from "react";
+import jsCookie from "js-cookie";
+import "./dashboard.css"
+import ScheudleTile from "./ScheduleTile";
+
+export default class Dashboard extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+
+        if (!jsCookie.get("access-token")) {
+
+            console.log("redirecting to login")
+            window.location.replace("/login");
+        }
+        return (
+            <div>
+                <p id="logout" className="navigation topRight" onClick={this.props.rightButtonHandler}>Logout</p>
+                <main className={"screen"}>
+                    <div className={"screenHeading"}>
+                        <h2>Dashboard</h2>
+                    </div>
+                    <div className={"screenMain"}>
+                        <div className={"tile"} id={"feeder"}>
+                            <h2 className={"tileHeading"}>Feeders</h2>
+                            {/*Make this its own component that also loads feeders*/}
+                            <ul className={"tileMain"}></ul>
+                            <div className={"tileFooter"}>
+                                {/*Make this its own component that also adds feeders*/}
+                                <i className={"fa-solid fa-plus add active"}></i>
+                            </div>
+                        </div>
+                        <div className={"tile"} id={"schedule"}>
+                            <h2 className={"tileHeading"}>Schedules</h2>
+                            {/*Make this its own component that also loads Schedules*/}
+                            <ScheudleTile/>
+                            <div className={"tileFooter"}>
+                                {/* TODO: Make this its own component that also adds Schedules*/}
+                                <i className={"fa-solid fa-plus add active"}></i>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        );
+    }
+}
